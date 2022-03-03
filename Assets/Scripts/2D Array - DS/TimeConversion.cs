cea350e57340c4666f16866f1e5ed6cb96eaaee1
Xtian2108 <christian_2108@live.com>
2022-03-02 22:48:42 -0500

    new way to do it

--- Assets/Scripts/2D Array - DS/TimeConversion.cs
+++ Assets/Scripts/2D Array - DS/TimeConversion.cs
@@ -7,8 +7,8 @@ public class TimeConversion : MonoBehaviour
 {
     // LINK OF PROBLEM: https://prnt.sc/3w84IbrEqePd
     // LINK OF PROBLEM: https://www.hackerrank.com/challenges/time-conversion/
-    
-    
+
+
     /*
      * Complete the 'timeConversion' function below.
      *
@@ -17,6 +17,7 @@ public class TimeConversion : MonoBehaviour
      */
 
     public string result;
+
     private void Start()
     {
         result = timeConversion("07:05:45PM");
@@ -27,32 +28,45 @@ public class TimeConversion : MonoBehaviour
     public static string timeConversion(string s)
     {
         char[] charList = s.ToCharArray();
-        
+
         int hour = int.Parse($"{charList[0]}{charList[1]}");
         int minute = int.Parse($"{charList[3]}{charList[4]}");
         int second = int.Parse($"{charList[6]}{charList[7]}");
-        
-        string AMPM = $"{charList[charList.Length-2]}{charList[charList.Length-1]}";
-        
-        if(AMPM == "PM")
+
+        string AMPM = $"{charList[charList.Length - 2]}{charList[charList.Length - 1]}";
+
+        if (AMPM == "PM")
         {
-            if(hour != 12)
+            if (hour != 12)
             {
-                hour += 12;                
+                hour += 12;
             }
         }
-        else{
-            if(hour >= 12){
-                hour -=12;
+        else
+        {
+            if (hour >= 12)
+            {
+                hour -= 12;
             }
         }
-        
+
         string result = string.Empty;
-        result= $"{hour.ToString("00")}:{minute.ToString("00")}:{second.ToString("00")}";
-        
-        
+        result = $"{hour.ToString("00")}:{minute.ToString("00")}:{second.ToString("00")}";
+
+
         return result;
+
+        
+
+         /*
+         
+         bool successfulconversion = DateTime.TryParse(s, out DateTime time);
+         if (successfulconversion)
+         {
+            return time.ToString("HH:mm:ss");
+         }
+         return "HackerRank gave me bad input";
+         
+         */
     }
-    
-    
 }
