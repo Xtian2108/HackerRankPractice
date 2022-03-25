893e0bb4f834195d5c8685e8c6ecdd9a6faa2566
Geta_Mac <christian_2108@live.com>
2022-03-24 23:12:38 -0500

    Number Of 1 Bits

---
+++ Assets/Scripts/Programming Skills/NumberOfOneBits.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Assets.Scripts.Programming_Skills
+{
+    public class NumberOfOneBits : MonoBehaviour
+    {
+        //https://leetcode.com/problems/number-of-1-bits/
+        public int HammingWeight(uint n) 
+        {
+            var count = 0;
+            while(n != 0)
+            {
+                if((n & 1) == 1)
+                    count++;
+                n = n >> 1;
+            }
+            return count;
+        }
+    }
+}
