68063575cb9f92cd3ac9d7cdea142f27ab269176
Christian Manuel Ñahues Cardozo <christian_2108@live.com>
2022-03-28 21:06:16 -0500

    Largest Perimeter Triangle

---
+++ Assets/Scripts/Programming Skills/LargestPerimeterTriangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Programming_Skills
+{
+    public class LargestPerimeterTriangle : MonoBehaviour
+    {
+        //https://leetcode.com/problems/largest-perimeter-triangle/
+        public int LargestPerimeter(int[] nums) 
+        {
+            Array.Sort(nums);
+            for(int i=nums.Length-1;i>=2;i--)
+            {
+                //Triangle Inequality
+                if(nums[i] < nums[i-1] + nums[i-2])
+                {
+                    return nums[i] + nums[i-1] + nums[i-2];
+                }
+            }
+            return 0;
+        }
+    }
+}
